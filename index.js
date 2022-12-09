2177b3dd160beba627b9bbfd84f631834971b8ff
developerpanda083 <cpandalee083@gmail.com>
2022-12-08 20:10:44 -0500

    Single play RPS done

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ function getComputerChoice () {
         return "Scissors"
     }
 }
+
+function playRound (playerChoice, compChoice) {
+        if (playerChoice === '1' && compChoice === "Paper") {
+            return "You lose! Paper beats rock."
+        }
+        else if (playerChoice === '1' && compChoice === "Scissors") {
+            return "You win! Rock beats scissors."
+        }
+        else if (playerChoice === '2' && compChoice === "Rock") {
+            return "You win! Paper beats rock."
+        }
+        else if (playerChoice === '2' && compChoice === "Scissors") {
+            return "You lose! Scissors beats paper."
+        }
+        else if (playerChoice === '3' && compChoice === "Rock") {
+            return "You lose! Rock beats scissors."
+        }
+        else if (playerChoice === '3' && compChoice === "Paper") {
+            return "You win! Scissors beats paper."
+        }
+        else {
+            return "You tied!"
+        }
+    
+}
+
+function game(userChoice, compChoice) {
+    for (let i = 0; i < 5; i++) {
+        playRound(userChoice, compChoice);
+    }
+}
