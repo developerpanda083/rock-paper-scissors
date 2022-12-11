1a6df557c7c5989785778f42a533068e0e285b7d
developerpanda083 <cpandalee083@gmail.com>
2022-12-11 17:35:31 -0500

    Created UI for RPS.

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 function getComputerChoice () {
     compNum = Math.floor(Math.random() * 60)
     if (compNum <= 20) {
-        return "Rock"
+        return 1;
+        //return "Rock"
     }
     else if (compNum <= 40) {
-        return "Paper"
+        return 2;
+        //return "Paper"
     }
     else {
-        return "Scissors"
+        return 3;
+        //return "Scissors"
     }
 }
 
 function playRound (playerChoice, compChoice) {
-        if (playerChoice === '1' && compChoice === "Paper") {
-            return "You lose! Paper beats rock."
+        if (playerChoice === 1 && compChoice === 2) {
+            return 2;
+            //return "You lose! Paper beats rock."
         }
-        else if (playerChoice === '1' && compChoice === "Scissors") {
-            return "You win! Rock beats scissors."
+        else if (playerChoice === 1 && compChoice === 3) {
+            return 1;
+            //return "You win! Rock beats scissors."
         }
-        else if (playerChoice === '2' && compChoice === "Rock") {
-            return "You win! Paper beats rock."
+        else if (playerChoice === 2 && compChoice === 1) {
+            return 2;
+            //return "You win! Paper beats rock."
         }
-        else if (playerChoice === '2' && compChoice === "Scissors") {
-            return "You lose! Scissors beats paper."
+        else if (playerChoice === 2 && compChoice === 3) {
+            return 3;
+            //return "You lose! Scissors beats paper."
         }
-        else if (playerChoice === '3' && compChoice === "Rock") {
-            return "You lose! Rock beats scissors."
+        else if (playerChoice === 3 && compChoice === 1) {
+            return 1;
+            //return "You lose! Rock beats scissors."
         }
-        else if (playerChoice === '3' && compChoice === "Paper") {
-            return "You win! Scissors beats paper."
+        else if (playerChoice === 3 && compChoice === 2) {
+            return 3;
+            //return "You win! Scissors beats paper."
         }
         else {
-            return "You tied!"
+            return 4;
+            //return "You tied!"
         }
     
 }
 
-function game(userChoice, compChoice) {
-    for (let i = 0; i < 5; i++) {
-        playRound(userChoice, compChoice);
-    }
-}
